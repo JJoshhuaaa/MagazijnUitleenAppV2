0f929320d6287c36b1d07cac0ce2cb8f61a3a4c3
JJoshhuaaa <83671662+JJoshhuaaa@users.noreply.github.com>
2024-10-30 10:42:02 +0100

    file-restructuring

---
+++ includes/header-section.php
@@ -0,0 +1,9 @@
+<nav>
+        <ul>
+            <li><a href="#section1">Go to Section 1</a></li>
+            <li><a href="#section2">Go to Section 2</a></li>
+            <li><a href="#section3">Go to Section 3</a></li>
+            <li><a href="#section4">Go to Section 4</a></li>
+            <li><a href="#section5">Go to Section 5</a></li>
+        </ul>
+    </nav>
