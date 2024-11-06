fe4df40d3cc9ca118ccdbf5ff475ce7a2be28a44
Elisei <144007909+Fl1ckJ@users.noreply.github.com>
2024-11-06 11:10:15 +0100

    products page

---
+++ frontend/products.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/products.css">
+</head>
+<body>
+<?php
+require '../includes/header-section.php';  
+?>
+<section>
+
+
+
+
+
+
+
+</section>
+
+
+<?php
+require '../includes/footer-section.php';
+?>
+</body>
+</html>
